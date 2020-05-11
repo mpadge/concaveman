bc90d8b5fa6be5ad33c5d37bf6f2755e1c572bf7
SymbolixAU <dcooley@symbolix.com.au>
2020-05-12 08:09:29 +1000

    templates and rearranged

--- src/concaveman.cpp
+++ src/concaveman.cpp
@@ -1,13 +1,11 @@
-#include "concaveman-src.h"
-#include "concaveman.h"
 #include "concaveman/api.hpp"
 
-//' rcpp_concaveman_mat
+//' rcpp_concaveman
 //' @noRd 
 // [[Rcpp::export]]
-Rcpp::DataFrame rcpp_concaveman_mat (Rcpp::NumericMatrix xy, Rcpp::IntegerVector hull_in,
+Rcpp::DataFrame rcpp_concaveman (SEXP xy, Rcpp::IntegerVector hull_in,
         const double concavity, const double length_threshold)
 {
-    return api::rcpp_concaveman_mat (xy, hull_in, concavity, length_threshold);
+    return concaveman::api::concaveman (xy, hull_in, concavity, length_threshold);
 }
 
